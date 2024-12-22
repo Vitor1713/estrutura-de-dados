3aec08db563992b3d5c86343f71aa2470026dd40
Vitor <vitor5g1713@gmail.com>
2024-12-22 16:59:22 -0300

    add: gitignore

--- stack.c
+++ stack.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct Stack Stack;
 
@@ -13,9 +14,8 @@ Stack* createStack() {
   Stack *stack = malloc(sizeof(Stack));
 
   stack->length = 0;
-  stack->values = malloc(sizeof(int));
+  stack->values = NULL;
   stack->top = malloc(sizeof(int));
-
   return stack;
 }
 
@@ -25,9 +25,13 @@ int length(Stack *stack) {
 
 void push(Stack *stack, int value) {
   stack->length++;
+  if(stack->length == 1) {
+    stack->values = malloc(stack->length * sizeof(int));
+  }else {
+    stack->values = realloc(stack->values, stack->length * sizeof(int));
+  }
   stack->values[stack->length - 1] = value;
-  stack->top = stack->values[stack->length - 1];
-  stack->values = realloc(stack->values, (stack->length + 1) * sizeof(int));
+  stack->top = &stack->values[stack->length - 1];
 } 
 
 int pop(Stack *stack) {
@@ -42,18 +46,43 @@ int *peek(Stack *stack) {
 
 int isEmpty(Stack *stack) {
   if(stack->length == 0) {
-    return 0;
+    return 1;
   }
+  return 0;
+}
+ 
+void printStack(Stack *stack) {
+  if(!isEmpty(stack)) {
+    for(int i = stack->length - 1; i >= 0; i--) {
+      printf("%i", stack->values[i]);
+      if(i == 0) {
+        continue;
+      }
+      printf(" - ");
+    }
+    printf("\nElemento do topo: %i", *peek(stack));
+    printf("\nNumero de elementos: %i", length(stack));
+  }else {
+    printf("Pilha vazia! \n"); 
+  }
+}
 
-  return 1;
+void populateStack(Stack *stack) {
+  int length, min, max = 0;
+  srand(time(NULL));
+  printf("Digite o tamanho da pilha: \n");
+  scanf("%i", &length);
+  printf("Digite um valor minimo: \n");
+  scanf("%i", &min);
+  printf("Digite um valor maximo: \n");
+  scanf("%i", &max);
+  for(int i = 0; i < length; i++) {
+    push(stack, rand() % max + min);
+  }
 }
 
 int main () {
-  Stack *stack = createStack(); 
-  push(stack, 30);
-  printf("tamanho: %i\n", stack->length);
-  printf("topo: %i", *stack->top);
+  Stack *stack = createStack();
+  populateStack(stack);
+  printStack(stack);
 }
-
-
-
